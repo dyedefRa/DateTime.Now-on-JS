1f8855aac0518cbd1ea9391833d40c5594f68d46
Baki Öztürk <41701856+dyedefRa@users.noreply.github.com>
2018-07-27 03:39:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
    function gunuAl() {
                 var today = new Date();
                 var dd = today.getDate();
-                var mm = today.getMonth() + 1; //January is 0!
+                var mm = today.getMonth() + 1; //Ocak için 0 geliyor o yüzden +1
                 var yyyy = today.getFullYear();
 
                 if (dd < 10) {
@@ -12,18 +12,18 @@
                     mm = '0' + mm
                 }
                 /////SAAT
-                var d = new Date(); // for now
-                saat = d.getHours(); // => 9
+                var d = new Date(); 
+                saat = d.getHours();   // Mesela 8 olsun;
                 if (saat < 10) {
-                    saat = '0' + saat;
+                    saat = '0' + saat;  // 08 gelir
                 }
-                dakika = d.getMinutes(); // =>  30
+                dakika = d.getMinutes(); //  11 icin
                 if (dakika < 10) {
-                    dakika = '0' + dakika;
+                    dakika = '0' + dakika;  // Buras yine 11
                 }
-                saniye = d.getSeconds(); // => 51
+                saniye = d.getSeconds(); //  3 oldugunu varsayalım
                 if (saniye < 10) {
-                    saniye = '0' + saniye;
+                    saniye = '0' + saniye; // 03 olur
                 }
                 tarih = dd + '/' + mm + '/' + yyyy;
                 saat = saat + ':' + dakika + ':' + saniye;
